9b09d776dabe8e10095246fccbccce3a45b2de2b
sumit <kus.sumit711204@gmail.com>
2020-02-22 19:43:02 +0530

    intial commit

---
+++ home.php
@@ -0,0 +1,263 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testing</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <link rel="stylesheet" type="text/css" href="style.css" media="all">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+		input[type=text] {
+     box-sizing: border-box;
+     border: 2px solid #ccc;
+     border-radius: 4px;
+     font-size: 16px;
+     background-color: white;
+      background-position: 10px 10px; 
+     background-repeat: no-repeat;
+     padding: 14px 4px 8px 15px;
+     -webkit-transition: width 0.4s ease-in-out;
+     transition: width 0.4s ease-in-out;
+    margin: 3px;
+     margin-top: 15px;
+    margin-left: -8px;
+}
+
+input[type=text]:focus {
+     width: 100%;
+}
+
+.sumit_ul li a:hover {
+       background: #000 !important;
+       color: #46ea1c;
+}
+
+button.sumit_but {
+     background-color: darkblue;
+      color: white;
+      width: 182px;
+}
+	</style>
+	
+</head>
+<body>
+  	<div class="Tools">
+	    <p class="one">Welcome to pro Construction Tools.
+	      <input type="submit" name value="LOGIN" style="float: right; padding: 11px;color:green; background:#151212; margin-top:0px;">
+	     	<input type="submit" name value="SIGNUP" style="float: right; padding: 11px;color: blue; background: #e0a7a7; margin-top:0px;"></p>
+	</div>
+<div class="logo">
+    <center>
+       <img src="images/tree.png" style="height: 51px;margin: 2px; float:left;">
+          <div class="text-block">
+ </div>
+  <center>
+	<div class="container">
+  <div class="dropdown">
+     <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">All Categories
+      <span class="caret"></span></button>
+       <ul class="dropdown-menu sumit_ul" role="menu" aria-labelledby="menu1">
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">FABRICS</a></li>
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">ROCKS</a></li>
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">BASH</a></li>
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CEMENT</a></li>
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">BRUSH</a></li>
+       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">ICE</a></li>
+      </ul>
+</div>
+<form>
+      <input type="text" name="search" placeholder="Search products.." >
+</form>
+      <button class="sumit_but" >Shopping Item<p>$0.00 0 items</p> <i class="fa fa-shopping-bag"></i></button>
+</div>
+</div>	
+<div class="topnav">
+    <a class="active" href="#home"><i class="w3-jumbo w3-spin fa fa-home" style="font-size:24px"></i></a>
+    <a href="#about">need</a>
+    <a href="#about">By Application</a>
+    <a href="#about">By industry</a>
+    <a href="#contact">Contact us</a>
+<div class="search-container">
+    <a class="active" href="#home"><i class="fa fa-heart" style="font-size:24px" ></i>Wishlist(4)</a>
+</form>
+</div>
+</center>
+ </div>
+      <div class="home"><center>
+ 	    <img src="images/tree.jpg" style="height: 494px; width: 1348px;">
+ 	    <div class="text-block">
+       <h4 style="font-size: 37px;">The K 535i is an excellent <br>addition to Husqvarna <br>construction is power cutter range</h4>
+        <p>- - - by sumit</p>
+</div>
+</center>
+</div>
+<!--content page start-->
+<section class="full_content">
+    <div class="left_pannel">
+    <div class="content">
+    <h2 style="border-bottom: 2px solid #383434;">Construction</h2><br>
+    <h5>wall saws</h5>
+    <h5>Ring Saws</h5>
+    <h5>Pro slab saws</h5>
+    <h5>Dry Blades</h5>
+    <h5>Asphalt Blades</h5>
+    <h5>Grinding wheels</h5>
+    <h5>Core Bits</h5>
+    <h5>Early entry blades</h5>
+    <h5>AOC - Asphalt ove concrete</h5>
+    <h5>Flooring</h5>
+</div>
+<div class="botto">
+    <h2 style="border-bottom: 2px solid #383434;">Line of Equipment:</h2><br>
+    <h5>wall saws</h5>
+</div>
+</div>
+<div class="right_pannel">
+<div data-bi-placement="Home_Row_5" class="row gutter">
+<!--start shop cat espot 1 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+   
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+<div class="h5-style-guide eco-ftr-title">Whirlpool 25 cu. ft. 36" Wide French Door Refrigerator
+    <br> <span style="font-weight: 700;"><center>price starting : $500</span>
+  <p><button style="color: white; background: red; margin-right: 79px;">Add to Cart</button></p></center>
+</div>
+</a>
+</div>
+<!--end shop cat espot 1 -->
+<!--start shop cat espot 2 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+         <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+<div class="h5-style-guide eco-ftr-title">Corry 6-Piece Leather Power Reclining Sofa
+        <br><span style="font-weight: 700;"><center>price starting : $2,299.99
+        <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+          </span>
+</div>
+</a>
+</div>
+<!--end shop cat espot 2 -->
+<div class="clearfix visible-xs visible-sm"></div>
+<!--start shop cat espot 3 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+    <div class="h5-style-guide eco-ftr-title">Featured Deals on Top <br>
+       Sellings PCs <br>
+     <span style="font-weight: 700;"><center>price Starting : $299.99
+      <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+      </span></div>
+</a>
+</div>
+<!--end shop cat espot 3 -->
+<!--start shop cat espot 4 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+    <div class="h5-style-guide eco-ftr-title">Lakeview Bathroom Vanities <br>by OVE
+    <br><span style="font-weight: 700;"><center>price starting : $4000
+       <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+    </span>
+</div>
+</a>
+</div>
+</div>	
+
+<div class="col-xs-6 col-md-3 eco-ftr">
+   
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+<div class="h5-style-guide eco-ftr-title">Whirlpool 25 cu. ft. 36" Wide French Door Refrigerator
+    <br> <span style="font-weight: 700;"><center>price starting : $500</span>
+  <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+</div>
+</a>
+</div>
+<!--end shop cat espot 1 -->
+<!--start shop cat espot 2 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+         <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+<div class="h5-style-guide eco-ftr-title">Corry 6-Piece Leather Power Reclining Sofa
+        <br><span style="font-weight: 700;"><center>price starting : $2,299.99
+        <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+          </span>
+</div>
+</a>
+</div>
+<!--end shop cat espot 2 -->
+<div class="clearfix visible-xs visible-sm"></div>
+<!--start shop cat espot 3 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+    <div class="h5-style-guide eco-ftr-title">Featured Deals on Top <br>
+       Sellings PCs <br>
+     <span style="font-weight: 700;"><center>price Starting : $299.99
+      <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+      </span></div>
+</a>
+</div>
+<!--end shop cat espot 3 -->
+<!--start shop cat espot 4 -->
+<div class="col-xs-6 col-md-3 eco-ftr">
+    <img class="img-responsive" src="images/rr.jpg" style="height: 104px;">
+    <div class="h5-style-guide eco-ftr-title">Lakeview Bathroom Vanities <br>by OVE
+    <br><span style="font-weight: 700;"><center>price starting : $4000
+       <p><button style="color: white; background: red;">Add to Cart</button></p></center>
+    </span>
+</div>
+</a>
+</div>
+</div>  
+</section>
+</div>
+<!--content page ends-->
+<p style="margin: 20px 71px 23px;">In areas of growth, house construction in the private sector has tended to focus on the production of traditional houses – either semi-separate family homes or larger separate executive homes. These private sector dwellings are beyond the reach of most first time buyers, particularly key-workers in comparatively low-paid jobs in the public sector. The Government is looking to housing associations to give dwellings for those who can’t afford to buy the house. The Housing Corporation is the key agency regulating . The Housing Corporation is the key agency regulating delivery of housing in the social housing sector. They assist with delivery of policy via grants awarded through the Approved Development Programme (ADP). A proportion of the ADP has been ring-fenced (The Challenge Fund) for the delivery of new homes quickly and cost effectively, in areas of high demand as well as stimulating a step change in supply by encouraging innovative forms of construction.</p>
+<center><h4 class="he_ader">Browser by popular construction Equipment Brands</h4></center>
+
+<div class="top_footer">
+  <img src="images/sponsered.jpg" width="185" height="100" style="float: right;" />
+</div>
+<div class="noo-vc-col col-md-10 col-sm-12 ">
+  <div class="wpb_raw_code wpb_content_element wpb_raw_html">
+    <div class="wpb_wrapper">
+      <!--marquee dir="left" behavour="scroll" delay="1" amount="5" onmouseover="this.stop();" onmouseout="this.start();">
+<img class="img-responsive" src="images/lo.png" >
+<img class="img-responsive" src="images/log.png" >
+<img class="img-responsive" src="images/logo.png">
+<img class="img-responsive" src="images/logoo.png">
+<img class="img-responsive" src="images/logooo.png">
+</marquee-->
+
+<marquee behavior="scroll" direction="left" scrollamount="15">
+    <img src="images/lo.png" width="220" height="110"  />
+    <img src="images/log.png" width="220" height="110"  />
+    <img src="images/logo.png" width="220" height="110" />
+    <img src="images/log.png" width="220" height="110"  />
+    <img  src="images/logoo.png" width="220" height="110" />
+    <img src="images/logooo.png" width="220" height="110" />
+     <img src="images/logoooo.jpg" width="220" height="110" />
+  </marquee>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="column" style="background-color:#aaa;">
+    
+    <p>  <center>policy : Return and Replacement &nbsp;| policy insurance shipping rates &nbsp;</center></p>
+  </div>
+  <div class="column" style="background-color:#aaa;">
+   
+    <p ><span class="question"><b>Questions?</b></span>&nbsp;&nbsp;&nbsp;&nbsp;<span>CALL US TOLL FREE (000) 1222 4667</span></p>
+   </div>
+</div>
+</span>
+<div class="footer">
+    <div class="logoo">
+     <center>
+       <img src="images/images.png" style="height:100px;margin: 2px; "></center>
+        </div>
+
+       <center> <p class="foo" style="color: white"><span>@2015-2019 pro construction Tools All Right Reserved</span></p
+        </center>
+    </div>
+</body>
+</html>
